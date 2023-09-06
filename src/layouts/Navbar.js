82dab48946e97eec20e88044c058cd4ef755810e
camaraanna <camara-anna@hotmail.fr>
2023-09-06 16:04:04 +0200

    store information

--- src/layouts/Navbar.js
+++ src/layouts/Navbar.js
@@ -1,8 +1,26 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useState, useContext } from "react";
 import logo from "../assets/img/Logo_clay.png";
+import { CartContext } from "../CartContext";
+import CartProduct from "../components/CartProduct";
 
 export const Navbar = () => {
+  const cart = useContext(CartContext);
+  const [show, setShow] = useState(false); // We initially have the modal not showing at first //
+
+  const handleClose = () => setShow(false); // If the close event happens or the hide event happens we hide the modal//
+
+  const handleShow = () => setShow(true);
+
+  // If we want to show the modal we set the show variable to true //
+  // We are Looping through the cart items and get the total amounts of all the different products quantities from our cart   //
+  const productsCount = cart.items.reduce((sum, product) => sum + product.quantity, 0); // With an arrow function we define the sum in the product so "whenever we go trough an item of our list we are going to reduce and go to that product and we are going to add a count up sum that running every single time" //
+
+  // We want the counter to start at 0 //
+
+  //reduce is gonna give us access to all the different products.quantities, summing up all the different product.quantities and then this entire statement is going to return a number  and we can access that number through productsCount//
+
   return (
     <div className="container_navbar">
       <div className="container-logo">
@@ -12,7 +30,11 @@ export const Navbar = () => {
           </figure>
         </div>
       </div>
-
+      <div>
+        {/* Here we add a onClick to handle show to store the show variable  */}
+        {/*  Our Navbar with productsCount is going to refresh and show the latest up-to-date product count value  */}
+        <button onClick={handleShow}>Cart ({productsCount} Items)</button>
+      </div>
       <div className="dropdown">
         <button className="dropbtn">
           <h3 className="dropbtn-text">Menu</h3>
